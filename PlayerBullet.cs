827cabdf3b34e0124224ba7681181f057373980d
KSTime1998 <90090509+KSTime1998@users.noreply.github.com>
2022-07-20 07:14:19 +0900

    플레이어 발사체 변수
    발사체의 데미지, 관통 여부, 레이저인지 여부

---
+++ PlayerBullet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public byte Damage;
+    public bool isPenetrate;
+    public bool isLazer;
+
+}
